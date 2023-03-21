61f5b2b92950fa959e136faea7d3aa146c74f76a
sekhar-1995 <subham742rath@gmail.com>
2023-03-21 19:00:08 +0530

    adding security group

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
 module "vpc" {
-  source          = "terraform-aws-modules/vpc/aws" // here we are saying t download terraform-aws-modules form "vpc" "aws" section
- 
-name            = var.VPC_NAME                    // we are taking the values of "name & cidr" from vars.tf file
-cidr            = var.VpcCIDR
-azs             = [var.Zone1, var.Zone2, var.Zone3]
+  source = "terraform-aws-modules/vpc/aws" // here we are saying t download terraform-aws-modules form "vpc" "aws" section
+
+  name            = var.VPC_NAME // we are taking the values of "name & cidr" from vars.tf file
+  cidr            = var.VpcCIDR
+  azs             = [var.Zone1, var.Zone2, var.Zone3]
   private_subnets = [var.PrivSub1CIDR, var.PrivSub2CIDR, var.PrivSub3CIDR]
   public_subnets  = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
 
